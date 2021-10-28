e106760c5351427b36a6dc2fb17a940da85a1e2f
jeffrey11lewis <54511741+jeffrey11lewis@users.noreply.github.com>
2021-10-28 12:27:57 -0500

    Update test_batchFileCreator.py

--- test_batchFileCreator.py
+++ test_batchFileCreator.py
@@ -4,8 +4,7 @@ import unittest
 file_exists = os.path.exists(r'C:\Users\torch\Desktop\clickHere.bat')
 
 class test_file_Exists(unittest.TestCase):
-    def test_file_exists(file_exists):
+    def test_file_exists(self):
 
-        file_exists.assertTrue(file_exists)
-
-#print(file_exists)
+        self.assertTrue(file_exists)
+print('file exists = ', file_exists)
